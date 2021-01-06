0600f035ca0f9a7558aa94ec26ac63c7fa234383
mihirrajkumar <77078237+mihirrajkumar@users.noreply.github.com>
2021-01-06 15:30:59 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,178 @@
+music.playMelody("G A B C5 G A B C5 ", 120)
+music.playMelody("G A B C5 G A B C5 ", 120)
+basic.forever(function () {
+    basic.showNumber(9)
+    basic.showNumber(21)
+    basic.showString("numbers")
+    basic.showString("zero")
+    basic.showLeds(`
+        . # # # .
+        . # . # .
+        . # . # .
+        . # . # .
+        . # # # .
+        `)
+    basic.showString("one")
+    basic.showLeds(`
+        . . # . .
+        . # # . .
+        . . # . .
+        . . # . .
+        . # # # .
+        `)
+    basic.showString("two")
+    basic.showLeds(`
+        . # # # #
+        . . . . #
+        . # # # #
+        . # . . .
+        . # # # #
+        `)
+    basic.showString("three")
+    basic.showLeds(`
+        . . # # #
+        . . . . #
+        . . # # #
+        . . . . #
+        . . # # #
+        `)
+    basic.showString("four")
+    basic.showLeds(`
+        # . . # .
+        # . . # .
+        # # # # #
+        . . . # .
+        . . . # .
+        `)
+    basic.showString("five")
+    basic.showLeds(`
+        # # # # #
+        # . . . .
+        # # # # #
+        . . . . #
+        # # # # #
+        `)
+    basic.pause(200)
+    basic.showString("six")
+    basic.showLeds(`
+        . . . # .
+        . . # . .
+        . # # # .
+        # . . . #
+        . # # # .
+        `)
+    basic.showString("seven")
+    basic.showLeds(`
+        # # # # #
+        . . . . #
+        . . . # .
+        . . # . .
+        . # . . .
+        `)
+    basic.showString("eight")
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # # # # #
+        # . . . #
+        # # # # #
+        `)
+    basic.showString("nine")
+    basic.showLeds(`
+        . # # # .
+        # . . . #
+        . # # # .
+        . . # . .
+        . # . . .
+        `)
+    basic.showString("ten")
+    basic.showLeds(`
+        # . # # #
+        # . # . #
+        # . # . #
+        # . # . #
+        # . # # #
+        `)
+    basic.showString("eleven")
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . # . # .
+        . # . # .
+        . # . # .
+        `)
+    basic.showString("twelve")
+    basic.showLeds(`
+        # . # # #
+        # . . . #
+        # . # # #
+        # . # . .
+        # . # # #
+        `)
+    basic.pause(100)
+    basic.showString("thirteen")
+    basic.showLeds(`
+        # . # # #
+        # . . . #
+        # . # # #
+        # . . . #
+        # . # # #
+        `)
+    basic.showString("fourteen")
+    basic.showLeds(`
+        # . # . #
+        # . # . #
+        # . # # #
+        # . . . #
+        # . . . #
+        `)
+    basic.showString("fifteen")
+    basic.showLeds(`
+        # . # # #
+        # . # . .
+        # . # # #
+        # . . . #
+        # . # # #
+        `)
+    basic.showString("sixteen")
+    basic.showLeds(`
+        # . . # .
+        # . # . .
+        # . # # #
+        # . # . #
+        # . # # #
+        `)
+    basic.pause(200)
+    basic.showString("Hello!")
+    basic.showLeds(`
+        # . . . .
+        # . # # #
+        # . . . #
+        # . . # .
+        # . # . .
+        `)
+    basic.showString("eighteen")
+    basic.showLeds(`
+        # . # # #
+        # . # . #
+        # . # # #
+        # . # . #
+        # . # # #
+        `)
+    basic.showString("nineteen")
+    basic.showLeds(`
+        # . # # #
+        # . # . #
+        # . # # #
+        # . . # .
+        # . # . .
+        `)
+    basic.showString("twenty")
+    basic.showLeds(`
+        # # # # #
+        . # # . #
+        # # # . #
+        # . # . #
+        # # # # #
+        `)
+})
